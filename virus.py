0e357ca9ff74131deafe61c21201c3c64a255d73
Thomas Lee <thomaslee9474@gmail.com>
2018-10-01 17:10:49 -0700

    corrected virus instantiation pytest

--- virus.py
+++ virus.py
@@ -1,6 +1,5 @@
 class Virus(object):
-
-# NOTE # *Properties and Attributes of the virus used in the Simulation
+    # NOTE # *Properties and Attributes of the virus used in the Simulation
 
     __init__(self, virus_name, mortality_rate, repro_rate):
         self.virus_name = virus_name
@@ -8,4 +7,8 @@ class Virus(object):
         self.basic_repro_num = basic_repro_num
     
     def test_virus_instantiation():
-        # *Check to make 
+        # *Check to make the virus instantiator is working 
+        virus =  Virus("HIV", 0.8, 0.3)
+        assert virus.name == "HIV"
+        assert virus.mortality_rate == 0.8
+        assert virus.basic_repro_num
